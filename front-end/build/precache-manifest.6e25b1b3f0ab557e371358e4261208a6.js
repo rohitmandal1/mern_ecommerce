7b652bd8980b3196406e313d422223a2cab6f8ec
Rohitmandal123 <emp91pam@engineerbabu.in>
2021-10-05 22:10:56 +0530

    update config

---
+++ front-end/build/precache-manifest.6e25b1b3f0ab557e371358e4261208a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ddc1489d8fee9785523537ac23ad1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1564aed45738fcb3f16d",
+    "url": "/static/css/main.5bd12691.chunk.css"
+  },
+  {
+    "revision": "91bc92e3758c54d3f886",
+    "url": "/static/js/2.ddfd7676.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.ddfd7676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1564aed45738fcb3f16d",
+    "url": "/static/js/main.6cfa6ff1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
